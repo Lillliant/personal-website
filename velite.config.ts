0536fd3263d8a164e1b9e1d3518454c9b3632c58
Christine Wong <55353492+Lillliant@users.noreply.github.com>
2026-09-08 22:04:26 -0400

    Add velite for tracking blogs

---
+++ velite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig, s } from "velite";
+
+export default defineConfig({
+  root: "content",
+  output: {
+    data: ".velite",
+    assets: "public/static",
+    base: "/static/",
+    clean: true,
+  },
+  collections: {
+    posts: {
+      name: "Post",
+      pattern: "posts/**/*.{md,mdx}",
+      schema: s
+        .object({
+          slug: s.slug("posts"), // Falls back to filename if not provided
+          title: s.string().max(120),
+          date: s.isodate(),
+          description: s.string().optional(),
+          body: s.mdx(),
+        })
+        .transform((data) => ({
+          ...data,
+          permalink: `/blog/${data.slug}`,
+        })),
+    },
+  },
+});
