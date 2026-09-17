75943eff4a3d0beea5d6c9a4ba1aa35feab9d65e
Christine Wong <55353492+Lillliant@users.noreply.github.com>
2026-09-17 14:20:31 -0400

    UI update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <main>
-      <div className="pt-24 pb-10 flex flex-col items-center justify-center px-4">
+      <div className="flex flex-col items-center justify-center px-4">
         <div className="h-50 w-50 overflow-hidden rounded-full bg-amber-200">
           <Image
             src="/avatar.png"
